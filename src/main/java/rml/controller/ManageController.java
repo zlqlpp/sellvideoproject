4454ad436ef72cb7a1f5c1c86bba9a3165fa44c8
zlqlpp <zlqlpp@gmail.com>
2019-11-12 13:59:17 +0800

    \

--- src/main/java/rml/controller/ManageController.java
+++ src/main/java/rml/controller/ManageController.java
@@ -355,7 +355,7 @@ class MusicImplements implements Runnable{
  
           processList = new ArrayList<String>();
 		try {
-			Process p = Runtime.getRuntime().exec("youtube-dl --e "+durl);
+			Process p = Runtime.getRuntime().exec("youtube-dl --get-filename "+durl);
 			p.waitFor();
 			BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			String line = "";
