0ac5232de12d041d56cf9bff36a8c22c7e7b1e93
zlq <zlqlpp@gmail.com>
2019-11-13 20:57:17 +0800

    1

--- src/main/java/rml/controller/MUserController.java
+++ src/main/java/rml/controller/MUserController.java
@@ -215,14 +215,14 @@ public class MUserController {
         try {
              
             reader = new BufferedReader(new FileReader(file));
-            String tempString = null;
+            String tempString = "";
             int line = 1;
             // 一次读入一行，直到读入null为文件结束
             while ((tempString = reader.readLine()) != null) {
                 // 显示行号
                 //System.out.println("line " + line + ": " + tempString);
                 passwdlist.add(tempString);
-                codeString+=tempString;
+                codeString+=","+tempString;
                 line++;
             }
             reader.close();
